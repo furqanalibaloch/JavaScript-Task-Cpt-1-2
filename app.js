506c1c7084e895f80002c947c7da7ead406082b6
furqanalibaloch <99679037+furqanalibaloch@users.noreply.github.com>
2022-10-29 22:26:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+// chapter 1
+// task 1
+
+// Question 1 
+// Declare a variable called username.
+
+var username;
+// task 2 
+// Q 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+var myname = "Furqan Ali"
+
+// task 3
+// Q 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var message;
+message = "Hello World";
+alert(message);
+
+// task 4
+// Q 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+var fullname = "Furqan Ali Lashari";
+var age = 28;
+var DOB = '08-02-1993';
+alert("Name : " + fullname + "\n" +"AGE : "+age +"\n"+"Date of birth: " + DOB);
+
+
+// task 5
+// Q 5. Write a script to display the following alert using one JS variable:
+
+window.alert("Pizza  \nPizz \nPiz  \nPi \nP")
+
+// task 6
+//Q 6 Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+var email = "furqanali592@gmail.com";
+
+alert("My email address is " + email);
+
+// task 7
+// Q 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+
+var book = "A smarter way to learn JavaScript";
+
+alert("I am trying to learn from the Book " + book);
+
+// task 8
+// Q 8. Write a script to display this in browser through JS
+
+var display = "Yah! I can write HTML content through JaveScript"
+document.write("<h1>" + display+ "</h1>");
+
+// task 9
+// Q 9. Store following string in a variable and show in alert and browser through JS
+
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”")
+
+
---
+++ chapter 2.js
@@ -0,0 +1,28 @@
+// chapter 2
+// Task 1
+
+// Q 1. Write a script to greet your website visitor using JS alert box.
+
+alert("Greeting ! Warm Welcome to our Website")
+
+// task 2
+// Q 2. Write a script to display following message on your web page:
+
+alert("Error! Please enter a valid Password");
+
+// task 3
+// Q 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+alert("Welcome to JS Land...\nHappy Coding!")
+
+// task 4
+// 4. Write a script to display following messages in sequence:
+
+alert("Welcome to JS Land");
+alert("Happy Coding!");
+
+
+// task 5
+// 5. Generate the following message through browser’s developer console:
+
+console.log("Hello... I can run JS through web browser's console")
